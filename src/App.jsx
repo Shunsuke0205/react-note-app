ef6c3b731d305f215c558b83a9aa8033bf31d7be
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2025-02-22 09:54:39 +0900

    impl: add and delete note functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
 import './App.css'
+import { useState } from 'react';
 import Main from './components/Main';
 import Sidebar from './components/Sidebar';
+import uuid from "react-uuid";
 
 function App() {
+
+  const [notes, setNotes] = useState([]);
+
+  const onAddNote = () => {
+    console.log('add node');
+    const newNote = {
+      id: uuid(),
+      title: "title of the note",
+      content: "content of the note",
+      modifiedDate: Date.now(),
+    };
+    setNotes([newNote, ...notes]);
+    console.log(notes);
+  };
+
+  const onDeleteNote = (deletingId) => {
+    const filteredNotes = notes.filter((note) => note.id !== deletingId); // filter() leaves the elements that pass the condition, note.id is not equal to deletingId.
+    setNotes(filteredNotes);
+  }
+
   return (
     <div className="App">
-      <Sidebar />
+      <Sidebar onAddNote={onAddNote} notes={notes} onDeleteNote={onDeleteNote} />
       <Main />
     </div>
   );
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,54 @@
 import React from "react";
 import "./Sidebar.css";
 
-const Sidebar = () => {
+const Sidebar = ({ onAddNote, notes, onDeleteNote }) => {
   return (
     <div className="app-sidebar">
       <div className="app-sidebar-header">
         <h1>note</h1>
-        <button>add</button>
+        <button onClick={onAddNote}>add</button>
       </div>
+
       <div className="app-sidebar-notes">
+        {notes.map((note) => (
+          <div className="app-sidebar-note" key={note.id}>
+            <div className="sidebar-note-title">
+              <strong>{note.title}</strong>
+              <button onClick={() => onDeleteNote(note.id)}>delete</button>
+            </div>
+            <p>{note.content}</p>
+            <small>last modified: {new Date(note.modifiedDate).toLocaleDateString("ja-JP", {
+              hour: "2-digit",
+              minute: "2-digit",
+              second: "2-digit",
+            })} </small>
+          </div>  
+        ))}
+
+        <div className="app-sidebar-note">
+          <div className="sidebar-note-title">
+            <strong>title</strong>
+            <button>delete</button>
+          </div>
+          <p>content</p>
+          <small>last modified: xx/xx</small>
+        </div>
+        <div className="app-sidebar-note">
+          <div className="sidebar-note-title">
+            <strong>title</strong>
+            <button>delete</button>
+          </div>
+          <p>content</p>
+          <small>last modified: xx/xx</small>
+        </div>
+        <div className="app-sidebar-note">
+          <div className="sidebar-note-title">
+            <strong>title</strong>
+            <button>delete</button>
+          </div>
+          <p>content</p>
+          <small>last modified: xx/xx</small>
+        </div>
         <div className="app-sidebar-note">
           <div className="sidebar-note-title">
             <strong>title</strong>
